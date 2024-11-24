560e12b2893088bd56c9102ae9f762623704e004
vikasvaish21 <vikasvash8@gmail.com>
2024-11-24 20:16:45 +0530

    Initial Commit

---
+++ StockAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockAssignment
+//
+//  Created by Vikas Vaish on 24/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
